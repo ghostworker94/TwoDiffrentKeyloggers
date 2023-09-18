585a05c584505204f582d129451f252f1e88eefb
Marshmellow <henke.teesalu@gmail.com>
2023-09-18 21:46:38 +0200

    Form1.Designer.cs

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,38 +1,42 @@
-﻿namespace Keyloggers;
-
-partial class Form1
+namespace Keyloggers
 {
-    /// <summary>
-    ///  Required designer variable.
-    /// </summary>
-    private System.ComponentModel.IContainer components = null;
-
-    /// <summary>
-    ///  Clean up any resources being used.
-    /// </summary>
-    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-    protected override void Dispose(bool disposing)
+    partial class Form1 : Form
     {
-        if (disposing && (components != null))
+        private Timer myTimer;
+
+        public Form1()
         {
-            components.Dispose();
+            InitializeComponent();
+
+            // Skapa en timer med en tidsintervall på 1000 millisekunder (1 sekund).
+            myTimer = new Timer();
+            myTimer.Interval = 1000;
+
+            // Lägg till en händelsehanterare för timerens Tick-händelse.
+            myTimer.Tick += new EventHandler(MyTimer_Tick);
+
+            // Starta timern när formuläret laddas.
+            myTimer.Start();
         }
-        base.Dispose(disposing);
+
+        private void MyTimer_Tick(object sender, EventArgs e)
+        {
+            // Denna funktion kommer att köras automatiskt varje sekund när timern tickar.
+            // Du kan lägga till din önskade funktionalitet här.
+            
+            // Exempel: Visa ett meddelande varje sekund.
+            MessageBox.Show("Programmet kör automatiskt!");
+        }
+
+        // Här kan du lägga till fler metoder och egenskaper efter behov.
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
     }
-
-    #region Windows Form Designer generated code
-
-    /// <summary>
-    ///  Required method for Designer support - do not modify
-    ///  the contents of this method with the code editor.
-    /// </summary>
-    private void InitializeComponent()
-    {
-        this.components = new System.ComponentModel.Container();
-        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.ClientSize = new System.Drawing.Size(800, 450);
-        this.Text = "Form1";
-    }
-
-    #endregion
 }
